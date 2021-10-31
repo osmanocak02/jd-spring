4a7fe1a23fbf2c5f9f93e93aac82776aba1ce65b
Osman Ocak <osmanocak02@yahoo.com>
2021-10-31 22:22:50 +0300

    loosely coupled

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,15 @@
+import implementation.Mentor;
+import service.FullTimeMentor;
+import service.MentorAccount;
+
+public class CybertekApp {
+
+    public static void main(String[] args) {
+
+        FullTimeMentor fullTime = new FullTimeMentor();
+
+        MentorAccount mentor = new MentorAccount(fullTime);
+
+        mentor.manageAccount();
+    }
+}
