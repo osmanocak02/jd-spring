887bf292ffde9e01913bb238889a272c10b97738
Osman Ocak <osmanocak02@yahoo.com>
2021-11-01 15:51:15 +0300

    DI-Constructor Injection second edition

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -5,6 +5,10 @@ import com.cybertek.interfaces.Course;
 public class Java implements Course {
     OfficeHours officeHours;
 
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
     @Override
     public void getTeachingHours() {
         System.out.println("Weekly Teaching Hours " + (20 + officeHours.getHours()));
