afc7aa48aad374879e295a7afb48a730124626aa
Osman Ocak <osmanocak02@yahoo.com>
2021-11-01 15:25:42 +0300

    DI-Constructor Injection

---
+++ Spring-02-DI-ConstructorInjection/src/main/java/CourseApp.java
@@ -0,0 +1,13 @@
+import com.cybertek.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CourseApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+
+        Course course = container.getBean("java", Course.class);
+
+        course.getTeachingHours();
+    }
+}
