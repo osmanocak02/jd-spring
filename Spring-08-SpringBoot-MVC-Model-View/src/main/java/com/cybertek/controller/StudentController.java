247409097a61f8015f4824acc164d30a339ba593
Osman Ocak <osmanocak02@yahoo.com>
2021-11-05 23:32:49 +0300

    Spring Boot MVC fifth edition

--- Spring-08-SpringBoot-MVC-Model-View/src/main/java/com/cybertek/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-Model-View/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,10 @@ public class StudentController {
 
         model.addAttribute("name", "Ozzy");
         model.addAttribute("course", "Java");
+        model.addAttribute("link", "www.google.com");
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
 
         return "student/welcome";
     }
