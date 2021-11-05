c4850be8a883a3cf281fe461384b75b09ac73d0c
Osman Ocak <osmanocak02@yahoo.com>
2021-11-05 14:06:35 +0300

    Spring Boot MVC fouth edition

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,6 +16,12 @@ public class HomeController {
 
     @RequestMapping("/ozzy")
     public String getRequestMapping2(){
+
+        return "home";
+    }
+
+    @GetMapping("/home/{name}")
+    public String pathVariablesEx(@PathVariable("name") String name){
 
         return "home";
     }
