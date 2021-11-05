bdbcac630e765953f0fa97900baea322b51d6d1e
Osman Ocak <osmanocak02@yahoo.com>
2021-11-06 00:44:49 +0300

    Spring Boot MVC Form created

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -0,0 +1,27 @@
+package com.cybertek.controller;
+
+import com.cybertek.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+@RequestMapping("/mentor")
+public class MentorController {
+
+    @GetMapping("/register")
+    public String showForm(Model model){
+
+        model.addAttribute("mentor", new Mentor());
+
+        List<String> batchList = Arrays.asList("87","88");
+
+        model.addAttribute("batchList", batchList);
+
+        return "mentor/mentor-register";
+    }
+}
