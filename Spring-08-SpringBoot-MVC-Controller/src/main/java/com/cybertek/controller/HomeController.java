c978888f248f57031a63e10e7cd96c272d7cce1d
Osman Ocak <osmanocak02@yahoo.com>
2021-11-05 13:22:15 +0300

    Spring Boot MVC third edition

--- Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-08-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,4 +1,20 @@
 package com.cybertek.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping
+    public String getRequestMapping(){
+
+        return "home";
+    }
+
+    @RequestMapping("/ozzy")
+    public String getRequestMapping2(){
+
+        return "home";
+    }
 }
