37551d10d68243b1d2a8885bd8338f63495ad774
Osman Ocak <osmanocak02@yahoo.com>
2021-11-06 14:23:44 +0300

    Spring Boot Employee Registration second edition

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -1,17 +1,35 @@
 package com.cybertek.controller;
 
+import com.cybertek.datagenerator.DataGenerator;
+import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
     @GetMapping("/register")
-    public String employeeCreate(){
+    public String employeeCreate(Model model){
 
+        model.addAttribute("employee", new Employee());
+
+        model.addAttribute("stateList", DataGenerator.getStateList());
 
         return "employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee){
+
+
+        return "employee/employee-list";
     }
 }
