ad0ce2efa338b9e79831b5bd252739596e74c46b
otto <otto.o@cybertekschool.com>
2021-11-23 12:47:13 +0300

    Spring Boot ORM OnetoMany version 2.0

--- Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/bootsrap/DataGenerator.java
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/cybertek/bootsrap/DataGenerator.java
@@ -1,5 +1,7 @@
 package com.cybertek.bootsrap;
 
+import com.cybertek.entity.Address;
+import com.cybertek.entity.Person;
 import com.cybertek.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +16,13 @@ public class DataGenerator implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        //Person
+        Person p1 = new Person("Mike","Smith");
+        Person p2 = new Person("Ozzy","Kmith");
+        Person p3 = new Person("Tedd","Bbith");
+
+        Address a1 = new Address("King St","22042");
+        Address a2 = new Address("Elm St","22102");
+        Address a3 = new Address("Java St","22036");
+
     }
 }
