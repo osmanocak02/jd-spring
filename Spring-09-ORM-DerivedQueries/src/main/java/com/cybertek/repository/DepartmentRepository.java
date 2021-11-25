512f19d0a06c123eab2da7271d6e48ab5c0f75aa
otto <otto.o@cybertekschool.com>
2021-11-25 22:20:47 +0300

    Spring Boot Derived Queries v2

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/DepartmentRepository.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/DepartmentRepository.java
@@ -2,6 +2,18 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department, String> {
+
+    List<Department> findAllByDepartment(String department);
+
+    List<Department> findAllByDivision(String division);
+
+    List<Department> findByDivisionEndsWith(String division);
+
+    List<Department> findDistinctTop3ByDivisionContains(String division);
 }
