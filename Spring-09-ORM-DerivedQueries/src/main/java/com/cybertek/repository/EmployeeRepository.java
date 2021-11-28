1d26549a98a21ea24951b44a23ac97d99ba6fe8a
otto <otto.o@cybertekschool.com>
2021-11-28 23:13:58 +0300

    Spring Boot JDQL started

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -2,10 +2,13 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
@@ -27,6 +30,25 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
 
     List<Employee> findByEmailIsNull();
+
+    @Query("select e from Employee e where e.email='ssymonds2@hhs.gov'")
+    Employee getEmployeeDetail();
+
+    @Query("select e.salary from Employee e where e.email='acurwood6@1und1.de'")
+    Integer getEmployeeSalary();
+
+    @Query("select e from Employee e where e.email=?1")
+    Optional<Employee> getEmployeeByEmail(String email);
+
+    @Query("select e from Employee e where e.email=?1 and e.salary=?2")
+    Optional<Employee> getEmployeeByEmailAndSalary(String email, int salary);
+
+    @Query("select e from Employee e where e.salary=:salary")
+    Optional<Employee> getEmployeeBySalary(@Param("salary") int salary);
+
+    @Query("select e from Employee e where e.firstName=:name or e.salary=:salary")
+    List<Employee> getEmployeeByFirstNameOrSalary(@Param("name") String firstName, @Param("salary") int salary);
+
 
 
 }
---
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/entity/Employee.java
@@ -0,0 +1,39 @@
+package com.cybertek.entity;
+
+import com.cybertek.enums.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "employees")
+@ToString
+public class Employee extends BaseEntity{
+
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate hireDate;
+
+    private Integer salary;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @ManyToOne
+    @JoinColumn(name = "department")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+}
