6f840f8f338451b86eb858d94161d891cdb78795
otto <otto.o@cybertekschool.com>
2021-12-10 14:58:11 +0300

    Spring Boot Security HttpBasic v4

---
+++ Spring-10-Security-Database/Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/bootsrap/DataGenerator.java
@@ -0,0 +1,31 @@
+package com.cybertek.bootsrap;
+
+import com.cybertek.entity.User;
+import com.cybertek.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DataGenerator implements CommandLineRunner {
+
+    private UserRepository userRepository;
+
+    public DataGenerator(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        this.userRepository.deleteAll();
+
+        User employee = new User("ozzy", "ozzy123", "USER", "");
+        User admin = new User("admin", "admin123", "ADMIN","ACCESS_TEST1,ACCESS_TEST2");
+        User manager = new User("manager", "manager1230", "MANAGER", "ACCESS_TEST1");
+
+        List<User> users = Arrays.asList(employee,admin,manager);
+
+        userRepository.saveAll(users);
+    }
+}
