5a5f3640cfeb3f5dbac11dda345c4943e9a7e2c6
otto <otto.o@cybertekschool.com>
2021-12-13 15:58:36 +0300

    Spring Boot Security Form Auth v7

--- Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/UserPrincipalDetailsService.java
+++ Spring-10-Security-FormAuth/src/main/java/com/cybertek/security/UserPrincipalDetailsService.java
@@ -19,8 +19,6 @@ public class UserPrincipalDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
-
-
         User user = userRepository.findByUsername(s);
 
         if (user==null){
