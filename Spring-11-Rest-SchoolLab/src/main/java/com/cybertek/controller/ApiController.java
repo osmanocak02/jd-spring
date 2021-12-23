3869ea4a5c6934923c89b32ac394febb7060f9e8
otto <otto.o@cybertekschool.com>
2021-12-23 16:54:04 +0300

    Spring Boot Rest School Lab v6

--- Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
+++ Spring-11-Rest-SchoolLab/src/main/java/com/cybertek/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.cybertek.controller;
 
 import com.cybertek.model.*;
+import com.cybertek.repository.AddressRepository;
 import com.cybertek.repository.ParentRepository;
 import com.cybertek.repository.StudentRepository;
 import com.cybertek.repository.TeacherRepository;
@@ -9,12 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApiController {
@@ -24,14 +23,18 @@ public class ApiController {
     //create repository
     //build method rest, get, object and consume it in address
 
-    @Autowired
+
     private TeacherRepository teacherRepository;
-
-    @Autowired
     private StudentRepository studentRepository;
-
-    @Autowired
     private ParentRepository parentRepository;
+    private AddressRepository addressRepository;
+
+    public ApiController(TeacherRepository teacherRepository, StudentRepository studentRepository, ParentRepository parentRepository, AddressRepository addressRepository) {
+        this.teacherRepository = teacherRepository;
+        this.studentRepository = studentRepository;
+        this.parentRepository = parentRepository;
+        this.addressRepository = addressRepository;
+    }
 
     @GetMapping("/teachers")
     public List<Teacher> readAllTeachers(){
@@ -53,6 +56,26 @@ public class ApiController {
         //return ResponseEntity.ok(responseWrapper);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseWrapper);
     }
+
+    @PutMapping("/address/{id}")
+    public Address updateTemperature(@PathVariable("id") long id, @RequestBody Address address) throws Exception {
+        Optional<Address> foundAddress = addressRepository.findById(id);
+
+        if(!foundAddress.isPresent()){
+            throw new Exception("This address could not find");
+        }
+
+        address.setCurrentTemperature(new Address().consumeTemp(address.getCity()));
+
+        address.setId(foundAddress.get().getId());
+
+        return addressRepository.save(address);
+    }
+
+
+
+
+
 
 
 
