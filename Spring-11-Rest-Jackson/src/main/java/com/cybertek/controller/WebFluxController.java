be7eb4acf9a20e4a0ec4fd6a767fce435d87331a
otto <otto.o@cybertekschool.com>
2021-12-24 11:55:39 +0300

    Spring Boot WebClient v2

--- Spring-11-Rest-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/controller/WebFluxController.java
@@ -1,9 +1,61 @@
 package com.cybertek.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.cybertek.entity.Genre;
+import com.cybertek.entity.MovieCinema;
+import com.cybertek.repository.GenreRepository;
+import com.cybertek.repository.MovieCinemaRepository;
+import com.cybertek.repository.MovieRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 public class WebFluxController {
 
-    //private
+    private WebClient webClient = WebClient.builder().baseUrl("http://localhost:8080").build();
+
+    private MovieCinemaRepository movieCinemaRepository;
+    private GenreRepository genreRepository;
+
+    public WebFluxController(MovieCinemaRepository movieCinemaRepository, GenreRepository genreRepository) {
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.genreRepository = genreRepository;
+    }
+
+    @GetMapping("/flux-movie-cinemas")
+    public Flux<MovieCinema> readAllCinemaFlux(){
+        return Flux.fromIterable(movieCinemaRepository.findAll());
+    }
+
+    @GetMapping("/mono-movie-cinema/{id}")
+    public Mono<MovieCinema> readById(@PathVariable("id") Long id){
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
+
+    @GetMapping("/mono-movie-cinema")
+    public Mono<MovieCinema> readByIdRequestParam(@RequestParam("id") Long id){
+        return Mono.just(movieCinemaRepository.findById(id).get());
+    }
+
+    @PostMapping("/create-genre")
+    public Mono<Genre> createGenre(@RequestBody Genre genre){
+        Genre createdGenre = genreRepository.save(genre);
+        return Mono.just((createdGenre));
+        //return Mono.just(genreRepository.save(genre));
+    }
+
+    @PutMapping("/update-genre")
+    public Mono<Genre> updateGenre(@RequestBody Genre genre){
+        Genre updatedGenre = genreRepository.save(genre);
+        return Mono.just(updatedGenre);
+    }
+
+    @DeleteMapping("/delete-genre/{id}")
+    public Mono<Void> deleteGenre(@PathVariable("id") Long id){
+        genreRepository.deleteById(id);
+        return Mono.empty();
+    }
 }
