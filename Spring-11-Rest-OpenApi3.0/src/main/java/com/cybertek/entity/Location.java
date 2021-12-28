c99817c2949aaa46cfd45b810354fb40ec3d0cf5
otto <otto.o@cybertekschool.com>
2021-12-28 16:31:16 +0300

    Spring Boot OpenAPI v1

---
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/entity/Location.java
@@ -0,0 +1,35 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.math.BigDecimal;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Location extends BaseEntity{
+
+    private String name;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String country;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String address;
+
+    public Location(String name, BigDecimal latitude, BigDecimal langitude, String country, String city, String state, String postalCode, String address) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = langitude;
+        this.country = country;
+        this.city = city;
+        this.state = state;
+        this.postalCode = postalCode;
+        this.address = address;
+    }
+}
