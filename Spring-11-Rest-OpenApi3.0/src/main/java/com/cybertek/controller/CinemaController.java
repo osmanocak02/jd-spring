924d52e8ef6c70a5bcfbc02102a7e03c2fb729d9
otto <otto.o@cybertekschool.com>
2021-12-29 14:54:59 +0300

    Spring Boot JWT v1

--- Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/controller/CinemaController.java
+++ Spring-11-Rest-OpenApi3.0/src/main/java/com/cybertek/controller/CinemaController.java
@@ -1,15 +1,19 @@
 package com.cybertek.controller;
 
 import com.cybertek.entity.Cinema;
+import com.cybertek.entity.User;
 import com.cybertek.repository.CinemaRepository;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cinema")
@@ -23,5 +27,42 @@ public class CinemaController {
     @Operation(summary = "Read all cinemas")
     public List<Cinema> readAll(){
         return cinemaRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Read one cinema by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Cinema.class))}),
+            @ApiResponse(responseCode = "400", description = "Something went wrong", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Cinema not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "No cinema value present", content = @Content)
+    })
+    public Cinema readById(@PathVariable("id") Long id) throws Exception {
+        Optional<Cinema> cinema = cinemaRepository.findById(id);
+        if (!cinema.isPresent()){
+            throw new Exception("This cinema is not found...");
+        }
+        return cinema.get();
+    }
+
+    @PostMapping("/create")
+    @Operation(summary = "Create cinema")
+    public Cinema create(@RequestBody Cinema cinema){
+        return cinemaRepository.save(cinema);
+    }
+
+    @PutMapping("/update/{id}")
+    @Operation(summary = "Update cinema")
+    public Cinema update(@PathVariable("id") long id, @RequestBody Cinema cinema){
+        Cinema newCinema = cinemaRepository.findById(id).get();
+        newCinema.setName(cinema.getName());
+        newCinema.setSponsoredName(cinema.getSponsoredName());
+        return cinemaRepository.save(newCinema);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete cinema")
+    public void delete(@PathVariable("id") long id){
+        cinemaRepository.deleteById(id);
     }
 }
