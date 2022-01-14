50482caaf4a7d47674632f51fad690ef4f3a04ca
otto <otto.o@cybertekschool.com>
2022-01-14 12:20:48 +0300

    Spring Boot

---
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/Spring09OrmMappingManytoManyApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09OrmMappingManytoManyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09OrmMappingManytoManyApplication.class, args);
+    }
+
+}
