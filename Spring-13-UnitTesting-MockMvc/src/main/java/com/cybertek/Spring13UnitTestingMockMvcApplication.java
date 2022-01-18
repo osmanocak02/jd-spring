80b139f40013cf08845be4180805dfb505e79a95
otto <otto.o@cybertekschool.com>
2022-01-18 09:33:52 +0300

    Spring Boot Mockito MVC v1

---
+++ Spring-13-UnitTesting-MockMvc/src/main/java/com/cybertek/Spring13UnitTestingMockMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cybertek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13UnitTestingMockMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring13UnitTestingMockMvcApplication.class, args);
+    }
+
+}
